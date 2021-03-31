b67611e579675397feab4f8bccfc4d4a2d1f3faa
ThatGuyWhoKilledYou <80376083+ThatGuyWhoKilledYou@users.noreply.github.com>
2021-03-31 01:02:38 -0400

    Add files via upload

---
+++ AsskickerV5.lua
@@ -0,0 +1,536 @@
+-- anyone need a plan for tonight?
+-- Version: 3.2
+
+-- Instances:
+
+local ScreenGui = Instance.new("ScreenGui")
+local Frame = Instance.new("Frame")
+local v5ass = Instance.new("TextLabel")
+local flip = Instance.new("TextButton")
+local voidtool = Instance.new("TextButton")
+local oompaloompa = Instance.new("TextButton")
+local _319y = Instance.new("TextButton")
+local PHV1 = Instance.new("TextButton")
+local misc = Instance.new("TextLabel")
+local TS2KIT = Instance.new("TextButton")
+local v1combat = Instance.new("TextButton")
+local reviz = Instance.new("TextButton")
+local freecam = Instance.new("TextButton")
+local _3cattach = Instance.new("TextButton")
+local map = Instance.new("TextButton")
+local es = Instance.new("TextButton")
+local sharky = Instance.new("TextButton")
+local spin = Instance.new("TextButton")
+local Shittervast = Instance.new("TextButton")
+local invisfling = Instance.new("TextButton")
+local eu = Instance.new("TextButton")
+local reset = Instance.new("TextButton")
+local bypass = Instance.new("TextButton")
+local cmdx = Instance.new("TextButton")
+local a6d = Instance.new("TextButton")
+local god = Instance.new("TextButton")
+local gothanims = Instance.new("TextButton")
+local elite = Instance.new("TextButton")
+local gz = Instance.new("TextButton")
+local TextButton = Instance.new("TextButton")
+
+--Properties:
+
+ScreenGui.Parent = game.CoreGui
+ScreenGui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+
+Frame.Parent = ScreenGui
+Frame.BackgroundColor3 = Color3.fromRGB(20, 74, 72)
+Frame.Position = UDim2.new(0.062956199, 0, 0.139442235, 0)
+Frame.Size = UDim2.new(0, 764, 0, 394)
+Frame.Active = true
+Frame.Draggable = true
+
+v5ass.Name = "v5ass"
+v5ass.Parent = Frame
+v5ass.BackgroundColor3 = Color3.fromRGB(30, 68, 139)
+v5ass.Position = UDim2.new(0.147553965, 0, 0, 0)
+v5ass.Size = UDim2.new(0, 540, 0, 50)
+v5ass.Font = Enum.Font.Bodoni
+v5ass.Text = "Asskicker V5"
+v5ass.TextColor3 = Color3.fromRGB(67, 255, 186)
+v5ass.TextScaled = true
+v5ass.TextSize = 14.000
+v5ass.TextWrapped = true
+
+flip.Name = "flip"
+flip.Parent = Frame
+flip.BackgroundColor3 = Color3.fromRGB(30, 68, 139)
+flip.Position = UDim2.new(0, 0, -5.96046448e-08, 0)
+flip.Size = UDim2.new(0, 111, 0, 50)
+flip.Font = Enum.Font.Bodoni
+flip.Text = "Backflip"
+flip.TextColor3 = Color3.fromRGB(67, 255, 186)
+flip.TextScaled = true
+flip.TextSize = 14.000
+flip.TextWrapped = true
+flip.MouseButton1Down:connect(function()
+	loadstring(game:HttpGet(('https://pastebin.com/raw/a6LWkwJL'),true))()
+end)
+
+voidtool.Name = "voidtool"
+voidtool.Parent = Frame
+voidtool.BackgroundColor3 = Color3.fromRGB(30, 68, 139)
+voidtool.Position = UDim2.new(0.85454154, 0, -1.93249434e-08, 0)
+voidtool.Size = UDim2.new(0, 111, 0, 50)
+voidtool.Font = Enum.Font.Bodoni
+voidtool.Text = "ToolVoid (Use Katana)"
+voidtool.TextColor3 = Color3.fromRGB(67, 255, 186)
+voidtool.TextScaled = true
+voidtool.TextSize = 14.000
+voidtool.TextWrapped = true
+voidtool.MouseButton1Down:connect(function()
+	loadstring(game:HttpGet(('https://pastebin.com/raw/u31PfgLM'),true))()
+end)
+
+oompaloompa.Name = "oompaloompa"
+oompaloompa.Parent = Frame
+oompaloompa.BackgroundColor3 = Color3.fromRGB(30, 68, 139)
+oompaloompa.Position = UDim2.new(0, 0, 0.154822335, 0)
+oompaloompa.Size = UDim2.new(0, 130, 0, 50)
+oompaloompa.Font = Enum.Font.Bodoni
+oompaloompa.Text = "Oompa Loompa"
+oompaloompa.TextColor3 = Color3.fromRGB(67, 255, 186)
+oompaloompa.TextScaled = true
+oompaloompa.TextSize = 14.000
+oompaloompa.TextWrapped = true
+oompaloompa.MouseButton1Down:connect(function()
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/ThatGuyWhoKilledYou/llo/main/Oompa_Loompa_1_18.txt", true))()
+end)
+
+_319y.Name = "319y"
+_319y.Parent = Frame
+_319y.BackgroundColor3 = Color3.fromRGB(30, 68, 139)
+_319y.Position = UDim2.new(0, 0, 0.301791787, 0)
+_319y.Size = UDim2.new(0, 129, 0, 50)
+_319y.Font = Enum.Font.Bodoni
+_319y.Text = "319y GUI"
+_319y.TextColor3 = Color3.fromRGB(67, 255, 186)
+_319y.TextScaled = true
+_319y.TextSize = 14.000
+_319y.TextWrapped = true
+_319y.MouseButton1Down:connect(function()
+	loadstring(game:HttpGet("https://pastebin.com/raw/WMf7hr2m", true))()
+end)
+
+PHV1.Name = "PHV1"
+PHV1.Parent = Frame
+PHV1.BackgroundColor3 = Color3.fromRGB(30, 68, 139)
+PHV1.Position = UDim2.new(0, 0, 0.463989705, 0)
+PHV1.Size = UDim2.new(0, 129, 0, 50)
+PHV1.Font = Enum.Font.Bodoni
+PHV1.Text = "Pornhub V1"
+PHV1.TextColor3 = Color3.fromRGB(67, 255, 186)
+PHV1.TextScaled = true
+PHV1.TextSize = 14.000
+PHV1.TextWrapped = true
+PHV1.MouseButton1Down:connect(function()
+	loadstring(game:HttpGet(('https://raw.githubusercontent.com/HamsterBiz/Pornhub-Gui/main/Pornhub'),true))()
+end)
+
+misc.Name = "misc"
+misc.Parent = Frame
+misc.BackgroundColor3 = Color3.fromRGB(30, 68, 139)
+misc.Position = UDim2.new(0, 0, 0.64974618, 0)
+misc.Size = UDim2.new(0, 764, 0, 31)
+misc.Font = Enum.Font.Bodoni
+misc.Text = "Miscellaneous"
+misc.TextColor3 = Color3.fromRGB(67, 255, 186)
+misc.TextScaled = true
+misc.TextSize = 14.000
+misc.TextWrapped = true
+
+TS2KIT.Name = "TS2KIT"
+TS2KIT.Parent = Frame
+TS2KIT.BackgroundColor3 = Color3.fromRGB(30, 68, 139)
+TS2KIT.Position = UDim2.new(0.191630751, 0, 0.461928904, 0)
+TS2KIT.Size = UDim2.new(0, 130, 0, 50)
+TS2KIT.Font = Enum.Font.Bodoni
+TS2KIT.Text = "TS2oolkit V1"
+TS2KIT.TextColor3 = Color3.fromRGB(67, 255, 186)
+TS2KIT.TextScaled = true
+TS2KIT.TextSize = 14.000
+TS2KIT.TextWrapped = true
+TS2KIT.MouseButton1Down:connect(function()
+	loadstring(game:HttpGet(('https://raw.githubusercontent.com/ThatGuyWhoKilledYou/toolkit/main/toolkit.txt'),true))()
+end)
+
+v1combat.Name = "v1combat"
+v1combat.Parent = Frame
+v1combat.BackgroundColor3 = Color3.fromRGB(30, 68, 139)
+v1combat.Position = UDim2.new(0.191963136, 0, 0.299492359, 0)
+v1combat.Size = UDim2.new(0, 130, 0, 50)
+v1combat.Font = Enum.Font.Bodoni
+v1combat.Text = "Mega Combat V1"
+v1combat.TextColor3 = Color3.fromRGB(67, 255, 186)
+v1combat.TextScaled = true
+v1combat.TextSize = 14.000
+v1combat.TextWrapped = true
+v1combat.MouseButton1Down:connect(function()
+	loadstring(game:HttpGet(('https://raw.githubusercontent.com/ThatGuyWhoKilledYou/plantpro/main/flyts2.txt'),true))()
+end)
+
+reviz.Name = "reviz"
+reviz.Parent = Frame
+reviz.BackgroundColor3 = Color3.fromRGB(30, 68, 139)
+reviz.Position = UDim2.new(0.191963136, 0, 0.154822335, 0)
+reviz.Size = UDim2.new(0, 130, 0, 50)
+reviz.Font = Enum.Font.Bodoni
+reviz.Text = "Reviz"
+reviz.TextColor3 = Color3.fromRGB(67, 255, 186)
+reviz.TextScaled = true
+reviz.TextSize = 14.000
+reviz.TextWrapped = true
+reviz.MouseButton1Down:connect(function()
+	loadstring(game:HttpGet(('https://pastebin.com/raw/KNUzQPYS'),true))()
+end)
+
+freecam.Name = "freecam"
+freecam.Parent = Frame
+freecam.BackgroundColor3 = Color3.fromRGB(30, 68, 139)
+freecam.Position = UDim2.new(0, 0, 0.769035518, 0)
+freecam.Size = UDim2.new(0, 146, 0, 91)
+freecam.Font = Enum.Font.Bodoni
+freecam.Text = "Freecam"
+freecam.TextColor3 = Color3.fromRGB(67, 255, 186)
+freecam.TextScaled = true
+freecam.TextSize = 14.000
+freecam.TextWrapped = true
+freecam.MouseButton1Down:connect(function()
+	loadstring(game:HttpGet(('https://raw.githubusercontent.com/ThatGuyWhoKilledYou/freecam/main/message%20(11).txt'),true))()
+end)
+
+_3cattach.Name = "3cattach"
+_3cattach.Parent = Frame
+_3cattach.BackgroundColor3 = Color3.fromRGB(30, 68, 139)
+_3cattach.Position = UDim2.new(0.373177052, 0, 0.154822335, 0)
+_3cattach.Size = UDim2.new(0, 130, 0, 50)
+_3cattach.Font = Enum.Font.Bodoni
+_3cattach.Text = "3Crowns Attach"
+_3cattach.TextColor3 = Color3.fromRGB(67, 255, 186)
+_3cattach.TextScaled = true
+_3cattach.TextSize = 14.000
+_3cattach.TextWrapped = true
+_3cattach.MouseButton1Down:connect(function()
+	loadstring(game:HttpGet(('https://raw.githubusercontent.com/ThatGuyWhoKilledYou/3c-attach/main/attach_backup_1.txt'),true))()
+end)
+
+map.Name = "map"
+map.Parent = Frame
+map.BackgroundColor3 = Color3.fromRGB(30, 68, 139)
+map.Position = UDim2.new(0.37421158, 0, 0.299492389, 0)
+map.Size = UDim2.new(0, 130, 0, 50)
+map.Font = Enum.Font.Bodoni
+map.Text = "D-MAP V2"
+map.TextColor3 = Color3.fromRGB(67, 255, 186)
+map.TextScaled = true
+map.TextSize = 14.000
+map.TextWrapped = true
+map.MouseButton1Down:connect(function()
+	loadstring(game:HttpGet(('https://pastebin.com/raw/10d034Mf'),true))()()
+end)
+
+
+es.Name = "es"
+es.Parent = Frame
+es.BackgroundColor3 = Color3.fromRGB(30, 68, 139)
+es.Position = UDim2.new(0.37421158, 0, 0.461928934, 0)
+es.Size = UDim2.new(0, 130, 0, 50)
+es.Font = Enum.Font.Bodoni
+es.Text = "Essence"
+es.TextColor3 = Color3.fromRGB(67, 255, 186)
+es.TextScaled = true
+es.TextSize = 14.000
+es.TextWrapped = true
+es.MouseButton1Down:connect(function()
+	loadstring(game:HttpGet("https://pastebin.com/raw/vU86kLYG"))()
+end)
+
+sharky.Name = "sharky"
+sharky.Parent = Frame
+sharky.BackgroundColor3 = Color3.fromRGB(30, 68, 139)
+sharky.Position = UDim2.new(0.560120761, 0, 0.299492389, 0)
+sharky.Size = UDim2.new(0, 130, 0, 50)
+sharky.Font = Enum.Font.Bodoni
+sharky.Text = "Sharky"
+sharky.TextColor3 = Color3.fromRGB(67, 255, 186)
+sharky.TextScaled = true
+sharky.TextSize = 14.000
+sharky.TextWrapped = true
+sharky.MouseButton1Down:connect(function()
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/SwissArmyTaco/V5/master/LOLFAG"))()
+end)
+
+spin.Name = "spin"
+spin.Parent = Frame
+spin.BackgroundColor3 = Color3.fromRGB(30, 68, 139)
+spin.Position = UDim2.new(0.81806308, 0, 0.461928964, 0)
+spin.Size = UDim2.new(0, 61, 0, 50)
+spin.Font = Enum.Font.Bodoni
+spin.Text = "Spin"
+spin.TextColor3 = Color3.fromRGB(67, 255, 186)
+spin.TextScaled = true
+spin.TextSize = 14.000
+spin.TextWrapped = true
+spin.MouseButton1Down:connect(function()
+	loadstring(game:HttpGet("https://pastebin.com/raw/6njC2ysa"))()
+end)
+
+Shittervast.Name = "Shittervast"
+Shittervast.Parent = Frame
+Shittervast.BackgroundColor3 = Color3.fromRGB(30, 68, 139)
+Shittervast.Position = UDim2.new(0.560120821, 0, 0.461928934, 0)
+Shittervast.Size = UDim2.new(0, 130, 0, 50)
+Shittervast.Font = Enum.Font.Bodoni
+Shittervast.Text = "Shittervast"
+Shittervast.TextColor3 = Color3.fromRGB(67, 255, 186)
+Shittervast.TextScaled = true
+Shittervast.TextSize = 14.000
+Shittervast.TextStrokeColor3 = Color3.fromRGB(67, 255, 186)
+Shittervast.TextWrapped = true
+Shittervast.MouseButton1Down:connect(function()
+	loadstring(game:HttpGet("https://pastebin.com/raw/AucE1f6E"))()
+end)
+
+invisfling.Name = "invisfling"
+invisfling.Parent = Frame
+invisfling.BackgroundColor3 = Color3.fromRGB(30, 68, 139)
+invisfling.Position = UDim2.new(0.91958189, 0, 0.299492389, 0)
+invisfling.Size = UDim2.new(0, 61, 0, 50)
+invisfling.Font = Enum.Font.Bodoni
+invisfling.Text = "Invisfling"
+invisfling.TextColor3 = Color3.fromRGB(67, 255, 186)
+invisfling.TextScaled = true
+invisfling.TextSize = 14.000
+invisfling.TextWrapped = true
+invisfling.MouseButton1Down:connect(function()
+	loadstring(game:HttpGet(('https://pastebin.com/raw/2fK67Cbt'),true))()
+end)
+
+eu.Name = "eu"
+eu.Parent = Frame
+eu.BackgroundColor3 = Color3.fromRGB(30, 68, 139)
+eu.Position = UDim2.new(0.558900654, 0, 0.154822335, 0)
+eu.Size = UDim2.new(0, 130, 0, 50)
+eu.Font = Enum.Font.Bodoni
+eu.Text = "EU GUI"
+eu.TextColor3 = Color3.fromRGB(67, 255, 186)
+eu.TextScaled = true
+eu.TextSize = 14.000
+eu.TextWrapped = true
+eu.MouseButton1Down:connect(function()
+	loadstring(game:HttpGet(('https://raw.githubusercontent.com/ThatGuyWhoKilledYou/cajunrecipes/main/EU.lua'),true))()
+end)
+
+reset.Name = "reset"
+reset.Parent = Frame
+reset.BackgroundColor3 = Color3.fromRGB(30, 68, 139)
+reset.Position = UDim2.new(0.920157015, 0, 0.461928934, 0)
+reset.Size = UDim2.new(0, 61, 0, 50)
+reset.Font = Enum.Font.Bodoni
+reset.Text = "Reset"
+reset.TextColor3 = Color3.fromRGB(67, 255, 186)
+reset.TextScaled = true
+reset.TextSize = 14.000
+reset.TextStrokeColor3 = Color3.fromRGB(67, 255, 186)
+reset.TextWrapped = true
+reset.MouseButton1Down:connect(function()
+	game.Players.LocalPlayer.Character.Head:Destroy()
+end)
+
+bypass.Name = "bypass"
+bypass.Parent = Frame
+bypass.BackgroundColor3 = Color3.fromRGB(30, 68, 139)
+bypass.Position = UDim2.new(0.274869114, 0, 0.769035518, 0)
+bypass.Size = UDim2.new(0, 146, 0, 91)
+bypass.Font = Enum.Font.Bodoni
+bypass.Text = "Chat Bypasser"
+bypass.TextColor3 = Color3.fromRGB(67, 255, 186)
+bypass.TextScaled = true
+bypass.TextSize = 14.000
+bypass.TextWrapped = true
+bypass.MouseButton1Down:connect(function()
+	loadstring(game:HttpGet('https://raw.githubusercontent.com/bedra45/chetbypasser/main/chetbypass'))()
+end)
+
+cmdx.Name = "cmdx"
+cmdx.Parent = Frame
+cmdx.BackgroundColor3 = Color3.fromRGB(30, 68, 139)
+cmdx.Position = UDim2.new(0.531413615, 0, 0.769035518, 0)
+cmdx.Size = UDim2.new(0, 146, 0, 91)
+cmdx.Font = Enum.Font.Bodoni
+cmdx.Text = "CMD X"
+cmdx.TextColor3 = Color3.fromRGB(67, 255, 186)
+cmdx.TextScaled = true
+cmdx.TextSize = 14.000
+cmdx.TextWrapped = true
+cmdx.MouseButton1Down:connect(function()
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/CMD-X/CMD-X/master/Source", true))()
+end)
+
+a6d.Name = "a6d"
+a6d.Parent = Frame
+a6d.BackgroundColor3 = Color3.fromRGB(30, 68, 139)
+a6d.Position = UDim2.new(0.819371879, 0, 0.154822335, 0)
+a6d.Size = UDim2.new(0, 137, 0, 50)
+a6d.Font = Enum.Font.Bodoni
+a6d.Text = "A6D (BROKE AS FUCK)"
+a6d.TextColor3 = Color3.fromRGB(67, 255, 186)
+a6d.TextScaled = true
+a6d.TextSize = 14.000
+a6d.TextWrapped = true
+a6d.MouseButton1Down:connect(function()
+	loadstring(game:HttpGet(('https://raw.githubusercontent.com/ThatGuyWhoKilledYou/A6D/main/A6D.lua'),true))()
+end)
+
+god.Name = "god"
+god.Parent = Frame
+god.BackgroundColor3 = Color3.fromRGB(30, 68, 139)
+god.Position = UDim2.new(0.819371879, 0, 0.299253643, 0)
+god.Size = UDim2.new(0, 61, 0, 50)
+god.Font = Enum.Font.Bodoni
+god.Text = "Godmode"
+god.TextColor3 = Color3.fromRGB(67, 255, 186)
+god.TextScaled = true
+god.TextSize = 23.000
+god.TextWrapped = true
+god.MouseButton1Down:connect(function()
+	loadstring(game:HttpGet(('https://pastebin.com/raw/7stffe0p'),true))()()
+end)
+
+gothanims.Name = "gothanims"
+gothanims.Parent = Frame
+gothanims.BackgroundColor3 = Color3.fromRGB(30, 68, 139)
+gothanims.Position = UDim2.new(0.807591796, 0, 0.769035578, 0)
+gothanims.Size = UDim2.new(0, 146, 0, 91)
+gothanims.Font = Enum.Font.Bodoni
+gothanims.Text = "Goths Animation GUI"
+gothanims.TextColor3 = Color3.fromRGB(67, 255, 186)
+gothanims.TextScaled = true
+gothanims.TextSize = 14.000
+gothanims.TextWrapped = true
+gothanims.MouseButton1Down:connect(function()
+	loadstring(game:HttpGet(('https://raw.githubusercontent.com/ThatGuyWhoKilledYou/gothanims/main/gothanimations.txt'),true))()
+end)
+
+elite.Name = "elite"
+elite.Parent = Frame
+elite.BackgroundColor3 = Color3.fromRGB(30, 68, 139)
+elite.Position = UDim2.new(0.739528894, 0, 0.15482235, 0)
+elite.Size = UDim2.new(0, 52, 0, 50)
+elite.Font = Enum.Font.Bodoni
+elite.Text = "Elite GUI"
+elite.TextColor3 = Color3.fromRGB(67, 255, 186)
+elite.TextScaled = true
+elite.TextSize = 14.000
+elite.TextWrapped = true
+elite.MouseButton1Down:connect(function()
+	loadstring(game:HttpGet(('https://pastebin.com/raw/qs0gwhgc'),true))()
+end)
+
+gz.Name = "gz"
+gz.Parent = Frame
+gz.BackgroundColor3 = Color3.fromRGB(30, 68, 139)
+gz.Position = UDim2.new(0.739528954, 0, 0.299492389, 0)
+gz.Size = UDim2.new(0, 52, 0, 50)
+gz.Font = Enum.Font.Bodoni
+gz.Text = "GZ GUI"
+gz.TextColor3 = Color3.fromRGB(67, 255, 186)
+gz.TextScaled = true
+gz.TextSize = 14.000
+gz.TextWrapped = true
+gz.MouseButton1Down:connect(function()
+	loadstring(game:HttpGet(('https://raw.githubusercontent.com/ThatGuyWhoKilledYou/GZZZZ/main/GZGUI.lua'),true))()
+end)
+
+TextButton.Parent = Frame
+TextButton.BackgroundColor3 = Color3.fromRGB(30, 68, 139)
+TextButton.Position = UDim2.new(0.739528954, 0, 0.461928934, 0)
+TextButton.Size = UDim2.new(0, 51, 0, 50)
+TextButton.Font = Enum.Font.Bodoni
+TextButton.Text = "Windows 98"
+TextButton.TextColor3 = Color3.fromRGB(67, 255, 186)
+TextButton.TextScaled = true
+TextButton.TextSize = 14.000
+TextButton.TextWrapped = true
+TextButton.MouseButton1Down:connect(function()
+	loadstring(game:HttpGet(('https://raw.githubusercontent.com/ThatGuyWhoKilledYou/windows98/main/Windows98.lua'),true))()
+end)
+
+-- Scripts:
+
+local function QMUUB_fake_script() -- ScreenGui.LocalScript 
+	local script = Instance.new('LocalScript', ScreenGui)
+
+	
+	--This script reveals ALL hidden messages in the default chat
+	--chat "/spy" to toggle!
+	enabled = true
+	--if true will check your messages too
+	spyOnMyself = true
+	--if true will chat the logs publicly (fun, risky)
+	public = false
+	--if true will use /me to stand out
+	publicItalics = true
+	--customize private logs
+	privateProperties = {
+		Color = Color3.fromRGB(0,255,255); 
+		Font = Enum.Font.SourceSansBold;
+		TextSize = 18;
+	}
+	--////////////////////////////////////////////////////////////////
+	local StarterGui = game:GetService("StarterGui")
+	local Players = game:GetService("Players")
+	local player = Players.LocalPlayer
+	local saymsg = game:GetService("ReplicatedStorage"):WaitForChild("DefaultChatSystemChatEvents"):WaitForChild("SayMessageRequest")
+	local getmsg = game:GetService("ReplicatedStorage"):WaitForChild("DefaultChatSystemChatEvents"):WaitForChild("OnMessageDoneFiltering")
+	local instance = (_G.chatSpyInstance or 0) + 1
+	_G.chatSpyInstance = instance
+	
+	local function onChatted(p,msg)
+		if _G.chatSpyInstance == instance then
+			if p==player and msg:lower():sub(1,4)=="/spy" then
+				enabled = not enabled
+				wait(0.3)
+				privateProperties.Text = "{SPY "..(enabled and "EN" or "DIS").."ABLED}"
+				StarterGui:SetCore("ChatMakeSystemMessage",privateProperties)
+			elseif enabled and (spyOnMyself==true or p~=player) then
+				msg = msg:gsub("[\n\r]",''):gsub("\t",' '):gsub("[ ]+",' ')
+				local hidden = true
+				local conn = getmsg.OnClientEvent:Connect(function(packet,channel)
+					if packet.SpeakerUserId==p.UserId and packet.Message==msg:sub(#msg-#packet.Message+1) and (channel=="All" or (channel=="Team" and public==false and Players[packet.FromSpeaker].Team==player.Team)) then
+						hidden = false
+					end
+				end)
+				wait(1)
+				conn:Disconnect()
+				if hidden and enabled then
+					if public then
+						saymsg:FireServer((publicItalics and "/me " or '').."{SPY} [".. p.Name .."]: "..msg,"All")
+					else
+						privateProperties.Text = "{SPY} [".. p.Name .."]: "..msg
+						StarterGui:SetCore("ChatMakeSystemMessage",privateProperties)
+					end
+				end
+			end
+		end
+	end
+	
+	for _,p in ipairs(Players:GetPlayers()) do
+		p.Chatted:Connect(function(msg) onChatted(p,msg) end)
+	end
+	Players.PlayerAdded:Connect(function(p)
+		p.Chatted:Connect(function(msg) onChatted(p,msg) end)
+	end)
+	privateProperties.Text = "{SPY "..(enabled and "EN" or "DIS").."ABLED}"
+	StarterGui:SetCore("ChatMakeSystemMessage",privateProperties)
+	local chatFrame = player.PlayerGui.Chat.Frame
+	chatFrame.ChatChannelParentFrame.Visible = true
+	chatFrame.ChatBarParentFrame.Position = chatFrame.ChatChannelParentFrame.Position+UDim2.new(UDim.new(),chatFrame.ChatChannelParentFrame.Size.Y)
+end
+coroutine.wrap(QMUUB_fake_script)()
